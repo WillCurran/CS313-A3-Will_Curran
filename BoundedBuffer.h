dcb16a44860495956e80348647a6f179bc81842d
WillCurran <willecurran@gmail.com>
2019-10-14 11:26:47 -0500

    1st attempt at bdd buf

--- BoundedBuffer.h
+++ BoundedBuffer.h
@@ -12,12 +12,17 @@ using namespace std;
 class BoundedBuffer
 {
 private:
-  	int cap;
+  	int cap; // capacity remaining
+    int full; // size of q. do we need this?
   	queue<vector<char>> q;
 
-	/* mutexto protect the queue from simultaneous producer accesses
-	or simultaneous consumer accesses */
+	/* mutex to protect the queue from simultaneous producer accesses
+	or simultaneous consumer accesses of q*/
 	mutex mtx;
+    /* mutex to use with slot_available*/
+    mutex m_sa;
+    /* mutex to use with data_available*/
+    mutex m_da;
 	
 	/* condition that tells the consumers that some data is there */
 	condition_variable data_available;
@@ -26,18 +31,40 @@ private:
 
 public:
 	BoundedBuffer(int _cap):cap(_cap){
-
+        full = 0;
 	}
 	~BoundedBuffer(){
 
 	}
 
 	void push(vector<char> data){
-		
+        unique_lock<mutex> l1(m_sa);
+        slot_available.wait(l1, []{return cap > 0;}); // wait until space to push
+        cap--;
+        full++;
+        l1.unlock();
+        
+        unique_lock<mtx> l2(mtx);
+        l2.lock();
+        q.push(data);
+        l2.unlock();
+        data_available.notify_one(); // wake up one thread to pop
 	}
 
 	vector<char> pop(){
 		vector<char> temp;
+        unique_lock<mutex> l1(m_da);
+        data_available.wait(l1, []{return full > 0}); // wait until we can pop (full = q.size()
+        cap++;
+        full--;
+        l1.unlock();
+        
+        unique_lock<mtx> l2(mtx);
+        l2.lock();
+        temp = q.front();
+        q.pop();
+        l2.unlock();
+        slot_available.notify_one(); // wake up one thread to push
 		return temp;  
 	}
 };
